e49c8591664a89967878b221f8bb7787554afdfd
Daniele Baccega <daniele.baccega@unito.it>
2022-05-06 16:30:13 +0200

    Introduced SSA traces

--- ProcessTrajectories.R
+++ ProcessTrajectories.R
@@ -1,19 +1,18 @@
-in_dir <- "data/"
-out_dir <- "dataset/data_inf_rate_0.00001-0.015/"
+in_dir <- "SEIR_analysis/"
+out_dir <- "dataset/Stochastic/data_inf_rate_0.00001-0.015/"
 
-load(paste0(in_dir, "SEIR-sensitivity.RData"))
+load(paste0(in_dir, "SEIR-analysis.RData"))
 
 listFiles <- list.files(in_dir, pattern = ".trace")
-
-S0 <- 255
 
 lapply(listFiles, function(x){
   data <- read.csv(paste0(in_dir, x), header=TRUE, sep="")
   
   ID <- strtoi(gsub(".*[-]([^.]+)[.].*", "\\1", x))
   
-  infection_rate <- config[[6]][[ID]][[3]]
-  recovery_rate <- config[[7]][[ID]][[3]]
+  S0 <- config[[1]][[ID]][[3]][[1]]
+  infection_rate <- config[[3]][[ID]][[3]]
+  recovery_rate <- config[[4]][[ID]][[3]]
   
   R0 <- (infection_rate * S0) / recovery_rate
     
