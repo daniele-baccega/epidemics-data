5aa826429d5a13e671f585d776780b4c755d4f06
Daniele Baccega <daniele.baccega@unito.it>
2022-05-10 23:33:32 +0200

    Added stochastic data

--- ProcessTrajectories.R
+++ ProcessTrajectories.R
@@ -5,6 +5,8 @@ load(paste0(in_dir, "SEIR-analysis.RData"))
 
 listFiles <- list.files(in_dir, pattern = ".trace")
 
+data_unique <- data.frame()
+
 lapply(listFiles, function(x){
   data <- read.csv(paste0(in_dir, x), header=TRUE, sep="")
   
@@ -14,9 +16,27 @@ lapply(listFiles, function(x){
   infection_rate <- config[[3]][[ID]][[3]]
   recovery_rate <- config[[4]][[ID]][[3]]
   
+  transpose <- t(data)
+  transpose <- as.data.frame(transpose)
+  rev_data_frame <- rev(transpose)
+  rev_data_frame <- t(rev_data_frame)
+  rev_data_frame <- as.data.frame(rev_data_frame)
+  
+  last_time <- rev_data_frame$Time[1] + 1
+  
+  f <- function(row){
+    #print(row["Time"])
+    if(last_time != row["Time"]){
+      data_unique <- rbind(data_unique, row)
+      last_time <- row["Time"]
+    }
+  }
+  
+  apply(rev_data_frame, 1, f)
+  
   R0 <- (infection_rate * S0) / recovery_rate
     
-  output <- append(data$I, R0)
+  output <- append(rev(rev_data_frame$I), R0)
   
   write.table(output, paste0(out_dir, x), col.names=FALSE, row.names=FALSE)
 })
